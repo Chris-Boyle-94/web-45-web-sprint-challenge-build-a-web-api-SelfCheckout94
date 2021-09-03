416935b8d9841df4ed27555968aefd31df2d86fb
Christopher Boyle <cboyle824@gmail.com>
2021-09-02 22:13:30 -0400

    set up get endpoints for / and /:id

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,28 @@
-// add middlewares here related to projects
+const { get } = require("./projects-model");
+
+const logger = (req, res, next) => {
+  console.log("METHOD: ", req.method);
+  next();
+};
+
+const validateProjectId = async (req, res, next) => {
+  try {
+    const project = await get(req.params.id);
+    if (project) {
+      req.project = project;
+      next();
+    } else {
+      next({
+        status: 404,
+        message: `No project with ID of ${req.params.id} was found`,
+      });
+    }
+  } catch (err) {
+    next;
+  }
+};
+
+module.exports = {
+  logger,
+  validateProjectId,
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,35 @@
-// Write your "projects" router here!
+const express = require("express");
+
+const { logger, validateProjectId } = require("./projects-middleware");
+
+const Projects = require("./projects-model");
+
+const router = express.Router();
+
+router.get("/", logger, async (req, res, next) => {
+  try {
+    const projects = await Projects.get();
+    if (projects.length > 0) {
+      res.status(200).json(projects);
+    } else {
+      res.status(200).send([]);
+    }
+  } catch (err) {
+    next();
+  }
+});
+
+router.get("/:id", logger, validateProjectId, async (req, res) => {
+  res.status(200).json(req.project);
+});
+
+// eslint-disable-next-line no-unused-vars
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    custom: "Something went wrong with the projects router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
